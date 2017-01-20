5d846160a05e9b92e26245e50ef19cefe74bb8e4
Daniel Lause <daniel.lause@retrobrain.de>
2017-01-21 00:42:53 +0100

    Added FadeBlack and UnitySingleton

---
+++ Assets/Scripts/Views/FadeBlackView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeBlackView : MonoBehaviour {
+
+    public CanvasGroup CanvasGroup { get; set; }
+
+    void Awake()
+    {
+        CanvasGroup = GetComponent<CanvasGroup>();
+    }
+
+}
