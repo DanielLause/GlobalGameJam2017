3addc0fa409c4d8082091cdbef2826934fad7836
mischa <mischa.ahi@gmx.de>
2017-01-20 22:44:24 +0100

    [UPDATE] First prototype level complete

---
+++ Assets/Scripts/Description.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Description : MonoBehaviour
+{
+    [TextArea]
+    public string DescriptionText;
+
+}
