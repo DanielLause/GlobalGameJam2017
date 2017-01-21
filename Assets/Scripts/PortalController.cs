a5f056237924ba6930ff34ca2273017177d6ea2a
Lukas <l.schuster1993@googlemail.com>
2017-01-21 01:51:18 +0100

    Added - PortalController and WaveObject Edited - Scripts

---
+++ Assets/Scripts/PortalController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour
+{
+    public Transform Start;
+    public Transform End;
+
+    public float DistanceToPortalToActivate;
+
+    private CharacterMovementController player;
+    private CameraController cameraController;
+    private bool isAbleToActivate = true;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<CharacterMovementController>();
+        cameraController = Camera.main.GetComponent<CameraController>();
+    }
+
+    private void Update()
+    {
+        CheckDistance();
+    }
+
+    private void CheckDistance()
+    {
+        if ((Vector3.Distance(player.transform.position, Start.position) <= DistanceToPortalToActivate) && isAbleToActivate)
+        {
+            isAbleToActivate = false;
+            StartCoroutine(Delay());
+            GameUIController.Instance.Fade();
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1);
+        player.transform.position = End.position;
+        cameraController.transform.position = new Vector3(player.transform.position.x, player.transform.position.y * cameraController.HeightAboveToFollow, player.transform.position.z + cameraController.DistanceFromToFollow);
+        cameraController.transform.LookAt(player.transform.position);
+        isAbleToActivate = true;
+    }
+}
